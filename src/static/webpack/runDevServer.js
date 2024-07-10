8f3596285d61ee32eacbfcf8bf7b4b9cf8f64ffb
satish <satishattada@gmail.com>
2024-07-10 17:38:24 +0100

    pused

--- src/static/webpack/runDevServer.js
+++ src/static/webpack/runDevServer.js
@@ -86,7 +86,7 @@ async function runExpressServer(state) {
     compress: false,
     client: {
       overlay: true,
-      logging: "warning",
+      logging: "warn",
     },
     ...state.config.devServer,
     hot: "only",

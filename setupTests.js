9adaa9094878ca102e77c36a00e47210160e5794
satish.attada <satish.attada@bp.com>
2025-12-01 18:09:00 +0000

    updated testcases

--- setupTests.js
+++ setupTests.js
@@ -1,9 +1,15 @@
 require('raf/polyfill')
 const { TextEncoder, TextDecoder } = require('util');
+const { ReadableStream, TransformStream } = require('stream/web');
+const { MessageChannel, MessagePort } = require('worker_threads');
 
-// Setup globals
+// Setup globals for web APIs required by undici/cheerio
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+global.ReadableStream = ReadableStream;
+global.TransformStream = TransformStream;
+global.MessageChannel = MessageChannel;
+global.MessagePort = MessagePort;
 
 const Enzyme = require('enzyme')
 const EnzymeAdapter = require('@cfaester/enzyme-adapter-react-18').default
